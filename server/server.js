83546360cf2a46d714245556220d8a847f0d3ba5
PrakashRajbanshi14 <prakashrajbanshi041@gmail.com>
2026-08-10 13:29:58 +0545

    learned to establish the socket connection

---
+++ server/server.js
@@ -0,0 +1,21 @@
+const http = require("http");
+const { Server } = require("socket.io");
+
+const server = http.createServer();
+
+const io = new Server(server, {
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"]
+    }
+});
+
+io.on("connection", (socket) => {
+    console.log("A user connected");
+
+    console.log("Socket ID:", socket.id);
+});
+
+server.listen(3000, () => {
+    console.log("Server running on port 3000");
+});
