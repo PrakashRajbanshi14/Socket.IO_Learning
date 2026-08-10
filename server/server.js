b946bdb2c83b90790de32460ff8f30788a00fa2e
PrakashRajbanshi14 <prakashrajbanshi041@gmail.com>
2026-08-10 13:54:27 +0545

    fix errors

--- server/server.js
+++ server/server.js
@@ -4,16 +4,21 @@ const { Server } = require("socket.io");
 const server = http.createServer();
 
 const io = new Server(server, {
-    cors: {
-        origin: "http://localhost:3000",
-        methods: ["GET", "POST"]
-    }
+  cors: {
+    origin: "http://127.0.0.1:5500",
+    methods: ["GET", "POST"],
+    allowedHeaders: ["Content-Type"]
+  }
 });
 
 io.on("connection", (socket) => {
     console.log("A user connected");
-
     console.log("Socket ID:", socket.id);
+    
+    // Listens for the client's "hello" event
+    socket.on("hello", (name) => {
+        console.log(`Hello event received from client ${name}!`);
+    });
 });
 
 server.listen(3000, () => {
